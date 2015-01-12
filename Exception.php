4d75379bf0312548980145005c4d87b1b42d1411
Marijn Ophorst <marijn@sensimedia.nl>
2015-01-12 17:08:30 +0100

    setup some exceptions

---
+++ Exception.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace cesession;
+
+abstract class Exception extends \Exception
+{
+}
+
---
+++ HandlerException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace cesession;
+
+class HandlerException extends Exception
+{
+}
+
