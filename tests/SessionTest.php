cb4dada4894fbe80dfcf7ed53d3a79bdbd51ce2e
Marijn Ophorst <marijn@sensimedia.nl>
2015-08-24 10:10:19 +0200

    TRY to set up some tests

--- tests/SessionTest.php
+++ tests/SessionTest.php
@@ -1,26 +1,54 @@
 <?php
 
 use Cesession\Session;
+use Cesession\Handler;
 
-class SessionTest extends PHPUnit_Framework_TestCase
+class SessionTest extends PHPUnit_Extensions_Database_TestCase
 {
-    public function testCreate()
+    static private $pdo = null;
+    private $conn = null;
+
+    protected function getSetUpOperation()
     {
-        $db = new PDO('sqlite::memory:');
-        $session = new Session;
-        $this->assertEquals(
-            1,
-            preg_match('@^[0-9a-zA-Z]{32}$@', $session->id())
-        );
-        return $session;
+        // Needed for truncates to cascade...
+        return PHPUnit_Extensions_Database_Operation_Factory::CLEAN_INSERT(true);
+    }
+    
+    public function getConnection()
+    {
+        if ($this->conn === null) {
+            $db = get_current_user().'_ft';
+            if (self::$pdo === null) {
+                //self::$pdo = new PDO('sqlite::memory:');
+                self::$pdo = new PDO('sqlite:bla');
+                self::$pdo->exec(file_get_contents(dirname(__DIR__).'/info/sql/sqlite.sql'));
+            }
+            $this->conn = $this->createDefaultDBConnection(self::$pdo, $db);
+        }
+        return $this->conn;
+    }
+    
+    public function getDataSet()
+    {
+        return $this->createXmlDataSet(__DIR__.'/data.xml');
     }
 
     /**
-     * @depends testCreate
+     * @outputBuffering enabled
      */
-    public function testSessionStarts(Session $session)
+    public function testSession()
     {
-        session_start();
+        $session = new Session('phpunit');
+        $session->registerHandler(new Handler\Pdo(self::$pdo));
+        @session_start();
+        $_SESSION['foo'] = 'bar';
+        session_write_close();
+        foreach (self::$pdo->query("SELECT * FROM cesession_session") as $sess) {
+            var_dump($sess);
+        }
+        @session_start();
+        var_dump($_SESSION);
+        var_dump(Session::$session);
     }
 }
 
