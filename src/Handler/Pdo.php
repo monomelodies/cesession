3bea0903ac3c86aadb9c6502da1c95bdc96d20cd
Marijn Ophorst <marijn@sensimedia.nl>
2015-08-24 09:16:34 +0200

    default Pdo handler

---
+++ src/Handler/Pdo.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Cesession\Handler;
+
+use Cesession\Handler;
+
+class Pdo implements Handler
+{
+    private $pdo;
+
+    public function __construct(\PDO $pdo)
+    {
+        $this->pdo = $pdo;
+    }
+    
+    public function read($id)
+    {
+        static $stmt;
+        if (!isset($stmt)) {
+            $stmt = $this->pdo->prepare(
+                "SELECT * FROM cesession_session WHERE id = :id"
+            );
+        }
+        $stmt->execute(compact('id'));
+        if ($data = $stmt->fetch(\PDO::FETCH_ASSOC)) {
+            return $data;
+        }
+        return false;
+    }
+    
+    public function write($id, $data)
+    {
+        static $create, $update;
+        if (!isset($create, $update)) {
+            $values = compact('id', 'data'); // Default.
+            foreach (self::$session as $key => $value) {
+                $values[$key] = $value;
+            }
+            $fields = [];
+            $placeholders = [];
+            $updates = [];
+            foreach ($values as $key => $value) {
+                $fields[] = $key;
+                $placeholders[] = ":$key";
+                if ($key != 'id') {
+                    $updates[] = "$key = :$key";
+                }
+            }
+            $create = $this->pdo->prepare(sprintf(
+                "INSERT INTO cesession_session (%s) VALUES (%s)",
+                implode(', ', $fields),
+                implode(', ', $placeholders)
+            ));
+            $update = $this->pdo->prepare(sprintf(
+                "UPDATE cesession_session SET %s WHERE id = :id",
+                implode(', ', $placeholders)
+            ));
+        }
+        // Try update first since that's the most common case.
+        $update->execute($values);
+        if ($affectedRows = $update->rowCount() and $affectedRows) {
+            return true;
+        }
+        $insert->execute($values);
+        return ($affectedRows = $insert->rowCount()) && $affectedRows;
+    }
+    
+    public function destroy($id)
+    {
+        static $stmt;
+        if (!isset($stmt)) {
+            $stmt = $this->pdo->prepace(
+                "DELETE FROM cesession_session WHERE id = :id"
+            );
+        }
+        $stmt->execute(compact('id'));
+        return ($affectedRows = $stmt->rowCount()) && $affectedRows;
+    }
+    
+    public function gc()
+    {
+        static $stmt;
+        if (!isset($stmt)) {
+            $stmt = $this->pdo->prepare(
+                "DELETE FROM cesession_session WHERE dateactive < ?"
+            );
+        }
+        $stmt->execute([date('Y-m-d H:i:s', strtotime('-45 minute'))]);
+        return ($affectedRows = $stmt->rowCount()) && $affectedRows;
+    }
+}
+
