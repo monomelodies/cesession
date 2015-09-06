3324301c7611406c42532574cf9afa1155f39dca
Marijn Ophorst <marijn@sensimedia.nl>
2015-09-06 11:21:04 +0200

    use a trigger for this

--- info/sql/mysql.sql
+++ info/sql/mysql.sql
@@ -6,3 +6,13 @@ CREATE TABLE cesession_session (
     INDEX(dateactive)
 ) ENGINE='InnoDB' DEFAULT CHARSET='UTF8';
 
+DROP TRIGGER IF EXISTS cesession_session_before_update;
+DELIMITER $$
+CREATE TRIGGER cesession_session_before_update BEFORE UPDATE ON cesession_session
+FOR EACH ROW
+BEGIN
+    SET NEW.dateactive = NOW();
+END;
+$$
+DELIMITER ;
+
