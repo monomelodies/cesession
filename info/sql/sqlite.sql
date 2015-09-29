b95f1736c3496b2ed2b3312aed24efcf18df48c7
Marijn Ophorst <marijn@sensimedia.nl>
2015-09-29 10:41:44 +0200

    sqlite is a bit silly here...

--- info/sql/sqlite.sql
+++ info/sql/sqlite.sql
@@ -6,10 +6,10 @@ CREATE TABLE cesession_session (
 );
 CREATE INDEX cesession_session_dateactive_key ON cesession_session(dateactive);
 
-DROP TRIGGER IF EXISTS cesession_session_before_update;
-CREATE TRIGGER cesession_session_before_update BEFORE UPDATE ON cesession_session
+DROP TRIGGER IF EXISTS cesession_session_after_update;
+CREATE TRIGGER cesession_session_after_update AFTER UPDATE ON cesession_session
 FOR EACH ROW
 BEGIN
-    SET NEW.dateactive = CURRENT_TIMESTAMP;
+    UPDATE cesession_session SET dateactive = CURRENT_TIMESTAMP WHERE id = NEW.id;
 END;
 
