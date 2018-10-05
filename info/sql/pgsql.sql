848f7fbb9a2efa9b44bc6f1996fb29151cb593b9
Marijn Ophorst <marijn@sensimedia.nl>
2018-10-05 13:23:51 +0200

    fix typo and coding style

--- info/sql/pgsql.sql
+++ info/sql/pgsql.sql
@@ -6,13 +6,12 @@ CREATE TABLE cesession_session (
 );
 CREATE INDEX cesession_session_dateactive_key ON cesession_session(dateactive);
 
-CREATE OR REPLACE FUNCTION cesession_session_before_update()
-RETURNS "trigger" AS $$
+CREATE OR REPLACE FUNCTION cesession_session_before_update() RETURNS "trigger" AS $$
 BEGIN
     NEW.dateactive := NOW();
     RETURN NEW;
 END;
 $$ LANGUAGE 'plpgsql';
-CREATE TRIGGER cesessino_session_before_update BEFORE UPDATE ON cesession_session
-    FOR EACH ROW EXECUTE PROCEDURE cesession_session_before_update();
+
+CREATE TRIGGER cesession_session_before_update BEFORE UPDATE ON cesession_session FOR EACH ROW EXECUTE PROCEDURE cesession_session_before_update();
 
