a2e8655f9c623d274ea31104c5cfdb228a86a746
Marijn Ophorst <marijn@sensimedia.nl>
2018-10-05 13:34:12 +0200

    type hints, doccomments

--- src/Handler/Memcached.php
+++ src/Handler/Memcached.php
@@ -6,21 +6,37 @@ use Monolyth\Cesession\Handler;
 
 class Memcached implements Handler
 {
+    /** @var Memcached */
     private $mc;
+    /** @var bool */
     private $exists = false;
 
+    /**
+     * @param Memcached $mc
+     * @return void
+     */
     public function __construct(\Memcached $mc)
     {
         $this->mc = $mc;
     }
 
-    private function getKey($id = null)
+    /**
+     * Helper to get a formatted key for memcached based on a session ID.
+     *
+     * @param string $id Session ID, defaults to `session_id()`.
+     * @return string
+     */
+    private function getKey(string $id = null) : string
     {
         $id = $id ?: session_id();
         return sprintf('%s/session/%s', session_name(), $id);
     }
     
-    public function read($id)
+    /**
+     * @param string $id
+     * @return array|null
+     */
+    public function read(string $id) :? array
     {
         if ($data = $this->mc->get($this->getKey($id))
             and $data = json_decode($data, true)
@@ -28,10 +44,15 @@ class Memcached implements Handler
             return $data;
         }
         $this->exists = false;
-        return false;
+        return null;
     }
     
-    public function write($id, $data)
+    /**
+     * @param string $id
+     * @param array $data
+     * @return bool
+     */
+    public function write(string $id, array $data) : bool
     {
         $values = $data + compact('id'); // Default.
         $values['dateactive'] = date('Y-m-d H:i:s');
@@ -42,12 +63,23 @@ class Memcached implements Handler
         );
     }
     
-    public function destroy($id)
+    /**
+     * @param string $id
+     * @return bool
+     */
+    public function destroy(string $id) : bool
     {
         return $this->mc->delete($this->getKey());
     }
     
-    public function gc($maxlifetime)
+    /**
+     * Garabage collection is done by memcached internally, so this is
+     * actually just a stub.
+     *
+     * @param int $maxlifetime
+     * @return bool Always returns true.
+     */
+    public function gc(int $maxlifetime) : bool
     {
         // Handled automatically be Memcached.
         return true;
