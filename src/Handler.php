79d2faab8ab97cd797f310f662e684a242c55172
Marijn Ophorst <marijn@monomelodies.nl>
2020-01-09 07:42:49 +0100

    add doccomments

--- src/Handler.php
+++ src/Handler.php
@@ -7,9 +7,38 @@ namespace Monolyth\Cesession;
  */
 interface Handler
 {
+    /**
+     * Read the session identified by `$id`.
+     *
+     * @param string $id
+     * @return array|null Session data, or null.
+     */
     public function read(string $id) :? array;
+
+    /**
+     * Write the data in `$data` to the session identified by `$id`.
+     *
+     * @param string $id
+     * @param array $data
+     * @return bool True on success, else false.
+     */
     public function write(string $id, array $data) : bool;
+
+    /**
+     * Destroy the session identified by `$id`.
+     *
+     * @param string $id
+     * @return bool True on success, else false.
+     */
     public function destroy(string $id) : bool;
+
+    /**
+     * Perform garbage collection for all sessions older than `$maxlifetime`
+     * seconds.
+     *
+     * @param int $maxlifetime
+     * @return bool True on success, else false.
+     */
     public function gc(int $maxlifetime) : bool;
 }
 
